acf825e91a46ee202766f42cd36b7ede5ec10fb8
EzeSandes <jezesandes@gmail.com>
2025-09-02 18:40:08 -0300

    password-generator - added new features to enable different options

--- Begginner/password-generator/src/App.jsx
+++ Begginner/password-generator/src/App.jsx
@@ -3,8 +3,48 @@ import LoopIcon from '@mui/icons-material/Loop';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import './App.css';
 
+const MIN_LENGTH = 4;
+const MAX_LENGTH = 32;
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(4);
+  const [includeLowercase, setIncludeLowercase] = useState(true);
+  const [includeUppercase, setIncludeUppercase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSpecial, setIncludeSpecial] = useState(false);
+
+  function handleGeneratePassword() {
+    const numbers = '0123456789';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const special = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    let chars = '';
+
+    if (includeLowercase) chars += lowercase;
+    if (includeNumbers) chars += numbers;
+    if (includeUppercase) chars += uppercase;
+    if (includeSpecial) chars += special;
+
+    if (chars === '') {
+      alert('Please select al least one character type.');
+      return;
+    }
+
+    let newPassword = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIdx = Math.floor(Math.random() * chars.length);
+      newPassword += chars[randomIdx];
+    }
+
+    setPassword(newPassword);
+  }
+
+  function handleCopy() {
+    navigator.clipboard.writeText(password);
+    console.log('Copied!');
+  }
 
   return (
     <div className='app'>
@@ -12,12 +52,18 @@ function App() {
 
       <div className='app__container'>
         <div className='app__result__container'>
-          <p className='app__result'>abcdefg</p>
+          <p className='app__result'>{password}</p>
           <div className='app__result__container__button__container'>
-            <button className='app__result__container__button app__result__container__button--generate'>
+            <button
+              className='app__result__container__button app__result__container__button--generate'
+              onClick={handleGeneratePassword}
+            >
               <LoopIcon sx={{ fontSize: '2rem' }} />
             </button>
-            <button className='app__result__container__button app__result__container__button--copy'>
+            <button
+              className='app__result__container__button app__result__container__button--copy'
+              onClick={handleCopy}
+            >
               <ContentCopyIcon sx={{ fontSize: '2rem' }} />
             </button>
           </div>
@@ -27,9 +73,11 @@ function App() {
           <div className='app__options__container__length'>
             <input
               type='number'
-              min={4}
-              max={32}
+              min={MIN_LENGTH}
+              max={MAX_LENGTH}
               name='length'
+              value={length}
+              onChange={e => setLength(Number(e.target.value))}
               className='app__options__input app__options__input--length'
             />
             <span className='app__options__input__length__description'>
@@ -39,22 +87,46 @@ function App() {
 
           <div className='app__options__container__includes'>
             <div className='app__options__container__includes__content'>
-              <input type='checkbox' name='UpperCase' id='uppercase' />
-              <label htmlFor='Uppercase'>A-Z</label>
-            </div>
-
-            <div className='app__options__container__includes__content'>
-              <input type='checkbox' name='lowercase' id='lowercase' />
+              <input
+                type='checkbox'
+                name='lowercase'
+                id='lowercase'
+                checked={includeLowercase}
+                onChange={() => setIncludeLowercase(prev => !prev)}
+              />
               <label htmlFor='lowercase'>a-z</label>
             </div>
 
             <div className='app__options__container__includes__content'>
-              <input type='checkbox' name='numbers' id='numbers' />
+              <input
+                type='checkbox'
+                name='UpperCase'
+                id='uppercase'
+                checked={includeUppercase}
+                onChange={() => setIncludeUppercase(prev => !prev)}
+              />
+              <label htmlFor='Uppercase'>A-Z</label>
+            </div>
+
+            <div className='app__options__container__includes__content'>
+              <input
+                type='checkbox'
+                name='numbers'
+                id='numbers'
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers(prev => !prev)}
+              />
               <label htmlFor='lowercase'>0-9</label>
             </div>
 
             <div className='app__options__container__includes__content'>
-              <input type='checkbox' name='special' id='special' />
+              <input
+                type='checkbox'
+                name='special'
+                id='special'
+                checked={includeSpecial}
+                onChange={() => setIncludeSpecial(prev => !prev)}
+              />
               <label htmlFor='lowercase'>!@#$%^&*</label>
             </div>
           </div>
